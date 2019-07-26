6f4b65a768ffd6c2acb9be904912110fd4911583
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-26 13:51:16 -0400

    Gets project up to Lesson 5's state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "@karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "@richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "@tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: [],
+    screen: 'list'
+  }
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts)=>{
+        this.setState(()=>({
+          contacts
+        }))
+      })
   }
   removeContact = (contact) => {
     this.setState((currentState) => ({
@@ -30,14 +22,23 @@ class App extends Component {
         return c.id !== contact.id
       })
     }))
+
+    ContactsAPI.remove(contact)
   }
   render() {
+    const {screen} = this.state
     return (
       <div>
-        <ListContacts 
+        { screen === 'list' && (<ListContacts 
           contacts={this.state.contacts}
           onDeleteContact={this.removeContact}
-        />
+          onNavigate={() => {
+            this.setState(() => ({
+              screen: 'create'
+            }))
+          }}
+        />) }
+        { screen === 'create' && (<CreateContact />) }
       </div>
     );
   }
---
+++ src/CreateContact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class CreateContact extends React.Component {
+  render() {
+    return (
+      <div>Create Contact</div>
+    )
+  }
+}
+
+export default CreateContact
