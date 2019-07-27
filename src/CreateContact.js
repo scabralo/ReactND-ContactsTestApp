c93570f94dfd3ce15f8abc7832f0f069b9a25c72
Sergio Cabral <sergioantonio60@gmail.com>
2019-07-27 07:59:01 -0400

    Implements back button and ImageInput on CreateContact

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -5,7 +5,19 @@ import ImageInput from './ImageInput';
 class CreateContact extends React.Component {
   render() {
     return (
-      <div>Create Contact</div>
+      <div>
+        <Link
+          className='close-create-contact'
+          to='/' >
+            Close
+        </Link>
+        <form>
+          <ImageInput 
+            className='create-contact-avatar-input'
+            name='avatarURL'
+            maxHeight={64}/>
+        </form>
+      </div>
     )
   }
 }
